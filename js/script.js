2b8277fb439e6123b441a60985bd3ed66b0178e7
kaushikkowboy <kaushik.c555@gmail.com>
2017-06-18 03:28:41 -0700

    Mario Added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 var height = $( window ).height();
-$('#background').height(height); 
+var body = $('body');
+var mario_img = $('#mario img');
+var bricks = $('#bottomBrick');
+$('#background').height(height);
+left_value = 0;
+// Mario Key Down
+body.keydown(function() {
+	// Moving Speed
+	left_value = left_value-5; 
+    mario_img.attr("src","images/mario_walking.gif");
+    bricks.css('left',left_value);
+    console.log(left_value); 
+}).keyup(function(){
+    mario_img.attr("src","images/mario.png"); 
+});
