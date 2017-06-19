1b1c86bd3d379e0e465e68fa31a0d9bd89314584
Kaushik Chelladurai <kaushik.c555@gmail.com>
2017-06-19 17:34:35 +0530

    Adding Right/Left Key Functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
-var height = $( window ).height();
+var height = $(window).height();
 var body = $('body');
 var mario_img = $('#mario img');
 var bricks = $('#bottomBrick');
+var bg_nature = $('#bg_nature');
 $('#background').height(height);
 left_value = 0;
 // Mario Key Down
 body.keydown(function(event) {
-	// Moving Speed
-	left_value = left_value-3; 
-    mario_img.attr("src","images/mario1.png");
-    bricks.css('left',left_value);
-    console.log(type); 
-// Mario Key Up
-}).keyup(function(){
-    mario_img.attr("src","images/mario.png"); 
+    var keyCode = event.keyCode;
+    // var keyVal = event.key;
+    // Moving Speed
+    mario_img.attr("src", "images/mario1.png");
+    switch (keyCode) {
+        case 37:
+            //Moving Left
+            if (left_value < 0) {
+                left_value = left_value + 3;
+                bricks.css('left', left_value);
+                bg_nature.css('left', left_value);
+                console.log('left');
+            } else {
+                console.log('Reached maximum');
+            }
+            break;
+        case 38:
+            //Moving Up
+            console.log('Up'); 
+            break;
+        case 39:
+            // Moving Right
+            left_value = left_value - 3;
+            bricks.css('left', left_value);
+            bg_nature.css('left', left_value);
+            console.log('Right');
+            break;
+        case 40:
+            //Moving Up
+            console.log('Down');
+            break;
+        default:
+            console.log('Just Use your Keyboard arrow to Move Mario!')
+    }
+    // Mario Key Up
+}).keyup(function() {
+    mario_img.attr("src", "images/mario.png");
 });
